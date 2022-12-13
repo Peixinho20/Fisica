8817d6a6c3f6379f89e8fa1e296caa7cb0a46691
Administrador <administrador@lab14-if.local>
2022-12-13 11:18:22 -0300

    leitura do arquivo de hidrogenio

--- exemplo6.cpp
+++ exemplo6.cpp
@@ -8,15 +8,22 @@
 int main(void){
 	FILE *fp;
 	char simb[3]; //leitura do simbolo quimico
-	
-	//leitutra de um arquivo nesse endereço no modo texto (rt)
+	int pos; //Criada para dizer onde o cursor está	
+
+	//leitura de um arquivo nesse endereço no modo texto (rt)
+	//Abre o arquivo
 	fp = fopen("/home/administrador/Documentos/Camila/Fisica/database/H.MU","rt");
 	if(fp == NULL){
 		printf("Problema ao acessar o arquivo!\n");
 		exit(0);
 	} 
+	//lê e mostra da tela o primeiro simbolo
 	fscanf(fp,"%s",simb);
 	printf("simb: %s\n",simb);
+	//diz onde o cursor parou
+	pos = ftell(fp);
+	printf("pos: %d\n",pos);
+
 
 	fclose(fp);
 	return 0;
